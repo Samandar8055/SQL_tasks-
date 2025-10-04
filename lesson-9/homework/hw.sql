7b0163e964ade5525d3e68d0784ab8f9a62c01fe
Samandar8055 <98967765+Samandar8055@users.noreply.github.com>
2025-10-04 09:23:29 +0500

    Update hw.sql

--- lesson-9/homework/hw.sql
+++ lesson-9/homework/hw.sql
@@ -105,6 +105,120 @@ inner join Departments as d
 on e.DepartmentID != d.DepartmentID
 
 --Task 12
+select *from Orders
+select *from Products
+
+select p.ProductName, o.Quantity,p.StockQuantity
+from Orders as o
+inner join Products as p
+on o.ProductID = p.ProductID
+where o.Quantity > p.StockQuantity
+
+
+--Task 13
+select *from Customers
+select *from Sales
+
+select c.FirstName, s.ProductID
+from Customers as c
+inner join Sales as s
+on c.CustomerID = s.CustomerID
+where s.SaleAmount >500
+
+--Task 14
+select *from Courses
+select *from Enrollments
+select *from Students
+
+select s.Name, c.CourseName, e.EnrollmentID
+from Students as s
+inner join Enrollments as e
+on e.StudentID = s.StudentID
+inner join Courses as c
+on c.CourseID = e.CourseID
+
+--Task 15
+select *from Products
+select *from Suppliers
+
+select p.ProductName ,s.SupplierName
+from Products as p
+inner join Suppliers as s
+on p.SupplierID = p.SupplierID
+where s.SupplierName like 'Tech%'
+
+--Task 16
+select *from Orders
+select *from Payments
+
+select o.OrderID, p.PaymentMethod,p.Amount,o.TotalAmount
+from Orders as o
+inner join Payments as p
+on o.OrderID = p.OrderID
+where p.Amount < o.TotalAmount
+
+--Task 17 
+select *from Employees 
+select * from Departments
+
+select e.Name, d.DepartmentName
+from Employees as e
+inner join Departments as d
+on e.DepartmentID = d.DepartmentID
+
+--Task 18 
+select *from Products
+select *from Categories
+
+select p.ProductName, c.CategoryName
+from Products as p
+inner join Categories as c
+on p.Category = c.CategoryID
+where c.CategoryName like 'Electronics' or c.CategoryName like  'Furniture'
+
+--Task 19 
+select *from Sales
+select *from Customers
+
+select s.SaleID, c.FirstName, c.Country 
+from Sales as s
+inner join Customers as c 
+on s.CustomerID = c.CustomerID
+where c.Country like 'USA'
+
+--Task 20
+select *from Orders
+select *from Customers
+
+select o.OrderID, c.FirstName, c.Country 
+from Orders as o
+inner join Customers as c
+on o.CustomerID = c.CustomerID
+where c.Country like 'Germany' and o.OrderID > 100
+
+--Hard Level Task (5)
+--Task 21
+select *from Employees
+
+SELECT e1.EmployeeID AS Emp1_ID, e1.Name AS Emp1_Name, e1.DepartmentID AS Emp1_Dept,
+       e2.EmployeeID AS Emp2_ID, e2.Name AS Emp2_Name, e2.DepartmentID AS Emp2_Dept
+FROM Employees e1
+CROSS JOIN Employees e2
+WHERE e1.EmployeeID < e2.EmployeeID
+  AND e1.DepartmentID <> e2.DepartmentID;
+
+--Task 22
+select*from Payments
+select* from Orders
+select *from Products
+
+select pro.ProductName,pa.PaymentMethod,pro.Price
+from Payments as pa
+inner join Orders as o
+on pa.OrderID = o.OrderID
+inner join Products as pro
+on pro.ProductID = o.ProductID
+where pa.Amount != o.Quantity *pro.Price
 
 
 

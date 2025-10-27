a00a0e50280f3a149bf8b13d9ca5a65153892686
Samandar8055 <98967765+Samandar8055@users.noreply.github.com>
2025-10-27 20:52:01 +0500

    Update hw.sql

--- lesson-8/homework/hw.sql
+++ lesson-8/homework/hw.sql
@@ -1,1 +1,12 @@
+--task 1
+-- Count number of products in each category using PIVOT
+SELECT *
+FROM (
+    SELECT Category, ProductID
+    FROM Products
+) AS SourceTable
+PIVOT (
+    COUNT(ProductID)
+    FOR Category IN ([Electronics], [Accessories], [Furniture], [Stationery], [Tools], [Clothing])
+) AS PivotTable;
 

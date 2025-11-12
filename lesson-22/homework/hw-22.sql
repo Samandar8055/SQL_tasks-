294d0d4cadcbc8bc39398a5dd4da617b756c76f2
Samandar8055 <98967765+Samandar8055@users.noreply.github.com>
2025-11-12 11:41:12 +0500

    Update hw-22.sql

--- lesson-22/homework/hw-22.sql
+++ lesson-22/homework/hw-22.sql
@@ -103,3 +103,104 @@ FROM OneColumn;
 --Task 14
 select *from sales_data
 
+select Customer_name , Product_category ,N_Category
+from(
+
+select Customer_name , Product_category , 
+count(Product_category)over(partition by Customer_name) as N_Category
+from sales_data) as t
+where N_Category >=2
+
+--Task 15 non
+select *from sales_data
+
+
+select Customer_name, total_amount, avg(Total_amount)over(partition by Region) as AVG_region
+from sales_data
+
+--Task 16
+select*from sales_data
+
+select Customer_name, Total_amount, Region,
+dense_rank()over(partition by Region order by Total_amount desc ) as ranked
+from sales_data
+
+--Task 17
+select *from sales_data
+
+select Customer_Name, Total_amount , Order_date,
+sum(Total_amount)over(partition by Customer_Id order by Order_date rows unbounded preceding) Running_totoal
+from sales_data
+
+--Task 18 non
+select Product_Category, Total_amount,
+sum(Total_amount)over(partition by Product_category order by Order_date) as sum_total,
+order_date
+from sales_data
+
+--Task 19
+select *from sales_data
+
+select Customer_name, lag(Total_amount)over(partition by Customer_id order by order_date) as prious_sale,
+order_date
+from sales_data
+where total_amount > lag(Total_amount)over(partition by Customer_id order by order_date);
+
+--Task 20
+select Product_name,
+unit_price,
+avg_price
+from(
+
+select Product_name, unit_price,
+avg(unit_price)over(partition by Product_category) as avg_price
+from sales_data) as t
+where unit_price > avg_price
+
+--Task 21
+select *from Mydata
+
+SELECT
+    Id,
+    Grp,
+    Val1,
+    Val2,
+    CASE 
+        WHEN ROW_NUMBER() OVER (PARTITION BY Grp ORDER BY Id) = 1
+        THEN SUM(Val1 + Val2) OVER (PARTITION BY Grp)
+        ELSE NULL
+    END AS Tot
+FROM MyData
+ORDER BY Id;
+
+--method 2
+select * ,
+case
+when row_number()over(partition by grp order by id)=1
+then sum(Val1 + Val2) over(partition by grp)
+else Null
+end as total
+from myData
+
+--Task 22
+select *from TheSumPuzzle
+
+select ID, sum(Cost) as Cost, sum(Quantity) as Quantity
+from theSumPuzzle
+group by ID
+
+--Task 23
+select *from Seats 
+
+WITH SeatGaps AS (
+    SELECT
+        SeatNumber,
+        LAG(SeatNumber, 1, 0) OVER (ORDER BY SeatNumber) AS PrevSeat
+    FROM Seats
+)
+SELECT
+    PrevSeat + 1 AS GapStart,
+    SeatNumber - 1 AS GapEnd
+FROM SeatGaps
+WHERE SeatNumber - PrevSeat > 1
+ORDER BY GapStart;
